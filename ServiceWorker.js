d03348b34b2f5449b53e313833619133f394cedb
venikamen <56189948+venikamen@users.noreply.github.com>
2024-11-04 02:10:50 +0300

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Anfloppy Arts-Rooftop Survival Live-0.1";
+const contentToCache = [
+    "Build/RSLiveWeb.loader.js",
+    "Build/RSLiveWeb.framework.js.unityweb",
+    "Build/RSLiveWeb.data.unityweb",
+    "Build/RSLiveWeb.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
